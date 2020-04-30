eb1f9ede6dd2d4cd6ed47c6fd898d5034e2eeff8
Julian Andrej <andrej1@llnl.gov>
2020-04-30 16:39:03 -0700

    update

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+async function fetchCommitID(owner, repoName, pullNumber) {
+  const { commitID } = await gql(
+    `{
+        repository(owner: ${owner}, name: ${repoName}) {
+          pullRequest(number: ${pullNumber}) {
+            commits(last: 1) {
+              nodes {
+                commit {
+                  id
+               }
+             }
+           }
+         }
+      }
+    }`
+  );
+
+  return commitID;
+}
+
 try {
   const repoToken = core.getInput("repoToken");
   const eventName = core.getInput("eventName");
 
-  if (eventName === "issue_comment") {}
+  const gql = octokit.graphql.defaults({
+    headers: {
+      authorization: `token ${repoToken}`,
+    },
+  });
+
+  const payload = github.context.payload;
 
   console.log(`Event: ${eventName}`);
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`Action started for issue #${payload.issue.number}`);
+  const owner = payload.repository.owner.login;
+  const repoName = payload.repository.name;
+  const pullNumber = payload.issue.number;
 
-  console.log(`The event payload: ${payload}`);
+  if (eventName === "issue_comment") {
+    if (payload.comment.body === "LGTM") {
+      console.log(`comment id ${payload.comment.id}`);
+      // Find most recent commit id
+      const commitID = fetchCommitID(owner, repoName, pullNumber);
+      console.log(commitID);
+    }
+  }
+
+  if (eventName === "status") {
+    console.log(`commit id ${payload.comment.id}`);
+  }
+
+  console.log(`The event payload: ${JSON.stringify(payload)}`);
 } catch (error) {
   core.setFailed(error.message);
 }
