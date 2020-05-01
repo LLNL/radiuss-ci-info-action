318684211fa3ae5b60b2338be4e888bdc3dd6170
Adrien M. Bernede <bernede1@llnl.gov>
2020-05-01 08:52:25 -0700

    compiled

--- dist/index.js
+++ dist/index.js
@@ -19944,37 +19944,41 @@ module.exports = require("events");
 const core = __webpack_require__(827);
 const github = __webpack_require__(148);
 
-try {
-  const repoToken = core.getInput("repoToken");
-  const eventName = core.getInput("eventName");
+async function run() {
+  try {
+    const repoToken = core.getInput("repoToken");
+    const eventName = core.getInput("eventName");
 
-  const payload = github.context.payload;
+    const payload = github.context.payload;
 
-  console.log(`### Event: ${eventName}`);
-  console.log(`### The event payload: `);
-  console.log(`${JSON.stringify(payload)}`);
+    console.log(`### Event: ${eventName}`);
+    console.log(`### The event payload: `);
+    console.log(`${JSON.stringify(payload)}`);
 
-  const owner = payload.repository.owner.login;
-  const repoName = payload.repository.name;
+    const owner = payload.repository.owner.login;
+    const repoName = payload.repository.name;
 
-  if (eventName === "issue_comment") {
-    if (payload.comment.body === "LGTM") {
-      console.log(`--> New comment (ID): ${payload.comment.id}`);
-      const pullNumber = payload.issue.number;
+    if (eventName === "issue_comment") {
+      if (payload.comment.body === "LGTM") {
+        console.log(`--> New comment (ID): ${payload.comment.id}`);
+        const pullNumber = payload.issue.number;
+      }
+    }
+
+    if (eventName === "status") {
+      console.log(`--> New status for commit (SHA): ${payload.sha}`);
+    }
+
+    if (eventName === "push") {
+      console.log(`--> New commit (SHA): ${payload.after}`);
     }
   }
-
-  if (eventName === "status") {
-    console.log(`--> New status for commit (SHA): ${payload.sha}`);
+  catch (error) {
+    core.setFailed(error.message);
   }
-
-  if (eventName === "push") {
-    console.log(`--> New commit (SHA): ${payload.after}`);
-  }
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run()
 
 
 /***/ }),
